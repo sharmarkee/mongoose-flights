0c06f596d737d461f8d9f4e8d408966f9b741d93
sharmarkee <esharmarke2@gmail.com>
2023-08-08 14:24:06 -0500

    flights

---
+++ mongoose-flights/models/flight.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema;
+
+const destinationSchema = new Schema({
+    airport: {
+        type: String,
+        enum: ['AUS', 'DAL', 'LAX', 'SEA'],
+    },
+    arrival: {
+        type: Date, 
+        // default: function() {
+        // return new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+        // },
+    },
+});
+
+var flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United']
+    },
+    flightNo: {
+        type: Number, 
+        min: 10,
+        max: 9999,
+    },
+    departs: {
+        type: Date, 
+        default: function() {
+        return new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+        },
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DAL', 'LAX', 'SEA'],
+        default: 'SEA',
+    },
+    destinations: [destinationSchema]
+});
+
+
+module.exports = mongoose.model('Flight', flightSchema);
